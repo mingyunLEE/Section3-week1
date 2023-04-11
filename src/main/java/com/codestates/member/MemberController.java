55e3bd6c2f58343108aafed0669ae2d616b968b2
mingyunLEE <leemingyun@naver.com>
2023-04-11 14:09:27 +0900

    Add MemberController

--- src/main/java/com/codestates/member/MemberController.java
+++ src/main/java/com/codestates/member/MemberController.java
@@ -1,9 +1,39 @@
 package com.codestates.member;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/v1/members")
+@RequestMapping(value = "/v1/members", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class MemberController {
+    @PostMapping
+    public String postMember(@RequestParam("email") String email,
+                             @RequestParam("name") String name,
+                             @RequestParam("phone") String phone){
+        System.out.println("# email: " + email);
+        System.out.println("# nameL " + name);
+        System.out.println("# phone: " + phone);
+
+        String response =
+                "{\"" +
+                        "email\":\"" + email + "\"," +
+                        "\"name\":\"" + name + "\",\"" +
+                        "phone\":\"" + phone+
+                        "\"}";
+        return response;
+    }
+    @GetMapping("/{member-id}")
+    public String getMember(@PathVariable("member-id")long memberId){
+        System.out.println("# memberId: " + memberId);
+
+        return null;
+    }
+
+    @GetMapping
+    public String getMember() {
+        System.out.println("# get Members");
+
+        return null;
+    }
+
 }
